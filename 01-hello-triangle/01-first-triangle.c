d33326a68161728d69a85987470dc5e07dd99ec4
Rodolfo Andres Rivas Matta <rodolforivasmatta@gmail.com>
2024-07-14 09:19:51 -0400

    Copy boilerplate code for first triangle.

---
+++ 01-hello-triangle/01-first-triangle.c
@@ -0,0 +1,75 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <string.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+void createTriangle() {
+  GLfloat verticies[] = {
+    
+  };
+}
+
+int main(int argc, char* argv[]) {
+  // Initialize GLFW.
+  if (!glfwInit()) {
+    printf("GLFW initialization failed.\n");
+    glfwTerminate();
+    return 1;
+  }
+
+  // OpenGL version 3.3.
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  // No backward compatibility.
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Allow forward compatibility.
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  // Create window.
+  GLFWwindow* mainWindow =
+      glfwCreateWindow(WIDTH, HEIGHT, "Hello world!", NULL, NULL);
+  if (!mainWindow) {
+    printf("GLFW windows creation failed.\n");
+    glfwTerminate();
+    return 1;
+  }
+
+  // Get buffer size information.
+  int bufferWidth, bufferHeight;
+  glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+  // Set context for GLEW to use.
+  glfwMakeContextCurrent(mainWindow);
+
+  // Allow modern extensions features.
+  glewExperimental = GL_TRUE;
+
+  if (glewInit() != GLEW_OK) {
+    printf("GLEW initialization failed.\n");
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    return 1;
+  }
+
+  // Setup viewport size.
+  glViewport(0, 0, bufferWidth, bufferHeight);
+
+  // Main loop.
+  while (!glfwWindowShouldClose(mainWindow)) {
+    // Get and handle user input events.
+    glfwPollEvents();
+
+    // Clear the window.
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(mainWindow);
+  }
+
+  return 0;
+}
