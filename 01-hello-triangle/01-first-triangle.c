0faa11002cf1e4168876e542b1367fc0462a5751
Rodolfo Andrés Rivas Matta <rodolforivasmatta@gmail.com>
2025-11-04 15:18:10 -0500

    Readjust vertices variables.

--- 01-hello-triangle/01-first-triangle.c
+++ 01-hello-triangle/01-first-triangle.c
@@ -28,8 +28,11 @@ void main() {\n\
 }";
 
 void createTriangle() {
-  GLfloat vertices[] = {-1.0f, -1.0f, 0.0f, 1.0f, -1.0f,
-                        0.0f,  0.0f,  1.0f, 0.0f};
+  GLfloat vertices[] = {
+    -1.0f, -1.0f, 0.0f,
+     1.0f, -1.0f, 0.0f,
+     0.0f,  1.0f, 0.0f
+  };
 
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
